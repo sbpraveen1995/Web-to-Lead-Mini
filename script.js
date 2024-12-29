bf0a710cc57dd990e9a226d91a53dd3e2c0968bc
Praveen S B <101457432+sbpraveen1995@users.noreply.github.com>
2024-12-29 14:29:43 +0530

    cla

---
+++ script.js
@@ -0,0 +1,10 @@
+function beforesubmit(){
+    
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value",inputdate.value);// in string so convert to locale
+
+    let formattedDate = new Date(inputdate.value).toLocaleDateString(en-IN);
+    outputdate.value = formattedDate;
+  
+}
